42acd1530568ce65ed9b0c33686ad91e030018d7
LeNguyenVi1457 <23211tt3822@mail.tdc.edu.vn>
2025-10-19 10:56:52 +0700

    Thêm ET, DAL, BUS Loại Tài Khoản và Cập Nhật Form

---
+++ QuanLyNganHang/ET/ET_LoaiTaiKhoan.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ET
+{
+    public class ET_LoaiTaiKhoan
+    {
+        public string MaLoaiTK {  get; set; }
+
+        public string ChiTiet {  get; set; }
+
+        public DateTime NgayTao { get; set; }
+
+        public DateTime NgayCapNhat { get; set; }
+
+        public string TrangThai { get; set; }
+
+        public bool TinhTrangXoa { get; set; }
+
+        public ET_LoaiTaiKhoan(string maLoaiTK, string chiTiet, DateTime ngayTao, DateTime ngayCapNhat, string trangThai, bool tinhTrangXoa)
+        {
+            MaLoaiTK = maLoaiTK;
+            ChiTiet = chiTiet;
+            NgayTao = ngayTao;
+            NgayCapNhat = ngayCapNhat;
+            TrangThai = trangThai;
+            TinhTrangXoa = tinhTrangXoa;
+        }
+    }
+}
