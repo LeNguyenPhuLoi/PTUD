a809c02b636ba3a65974cd488472a429d3636ab8
KhanhDang <nkdan2005@gmail.com>
2025-10-30 08:27:50 +0700

    Thêm ET

---
+++ QuanLyNganHang/ET/ET_BienLai.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ET
+{
+    public class ET_BienLai
+    {
+        public ET_BienLai(string maBL, string maGD, string maKH, string maTK, string maNV, decimal soTien, string maNgoaiTe, string loaiBL, string moTa, string trangThai, string tinhTrangXoa)
+        {
+            MaBL = maBL;
+            MaGD = maGD;
+            MaKH = maKH;
+            MaTK = maTK;
+            MaNV = maNV;
+            SoTien = soTien;
+            MaNgoaiTe = maNgoaiTe;
+            LoaiBL = loaiBL;
+            MoTa = moTa;
+            TrangThai = trangThai;
+            TinhTrangXoa = tinhTrangXoa;
+        }
+
+        public string MaBL {  get; set; }
+        public string MaGD { get; set; }
+        public string MaKH { get; set; }
+        public string MaTK { get; set; }
+        public string MaNV { get; set; }
+        public decimal SoTien { get; set; }
+        public string MaNgoaiTe { get; set; }
+        public string LoaiBL { get; set; }
+        public string MoTa {  get; set; }
+        public string TrangThai {  get; set; }
+        public string TinhTrangXoa { get; set; }
+    }
+}
---
+++ QuanLyNganHang/ET/ET_TraNo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ET
+{
+    public class ET_TraNo
+    {
+        public ET_TraNo(string maTraNo, string maVay, decimal soTienNo, decimal soTienTra, DateTime ngayTra, string tinhTrangXoa)
+        {
+            MaTraNo = maTraNo;
+            MaVay = maVay;
+            SoTienNo = soTienNo;
+            SoTienTra = soTienTra;
+            NgayTra = ngayTra;
+            TinhTrangXoa = tinhTrangXoa;
+        }
+
+        public string MaTraNo { get; set; }
+        public string MaVay { get; set; }
+        public decimal SoTienNo { get; set; }
+        public decimal SoTienTra {  get; set; }
+        public DateTime NgayTra {  get; set; }
+        public string TinhTrangXoa { get; set; }
+    }
+}
